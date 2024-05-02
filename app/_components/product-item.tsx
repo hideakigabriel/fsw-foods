9264e9547948ea6c821667fba8f8effadb12018f
Hideaki Gabriel <hideaki.gabriel@gmail.com>
2024-05-02 00:11:04 -0300

    fix: arrow icon inside div container

--- app/_components/product-item.tsx
+++ app/_components/product-item.tsx
@@ -30,8 +30,8 @@ export const ProductItem = ({ product }: ProductItemProps) => {
 
         {product.discountPercentage && (
           <div className="absolute left-2 top-2 gap-[2px]">
-            <ArrowDownIcon size={12} />
             <span className="flex items-center rounded-full bg-primary px-2 py-[2px] text-xs font-semibold text-white">
+              <ArrowDownIcon size={12} />
               {product.discountPercentage}%
             </span>
           </div>
